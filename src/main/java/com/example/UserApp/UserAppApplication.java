e67bc74286f511afa94750242c2a8a95434c88bf
vivekkumarmaurya <vivekkumarmaurya97@gmail.com>
2023-07-02 10:28:40 +0530

    code upload

---
+++ src/main/java/com/example/UserApp/UserAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.UserApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserAppApplication.class, args);
+	}
+
+}
